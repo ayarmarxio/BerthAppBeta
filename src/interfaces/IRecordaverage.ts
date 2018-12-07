f89c5d6bef5584e02864873cf6be81fa94f852d1
ayarmarxio <ayarmarxio@gmail.com>
2018-12-07 11:50:31 +0100

    some changes

---
+++ src/interfaces/IRecordaverage.ts
@@ -0,0 +1,14 @@
+export default interface IRecordaverage {
+  day: number;
+  month: number;
+  bpSystolic: number;
+  bpDiastolic: number;
+  bodyTemperature: number;
+  heartBeatPerSecond: number;
+  dust: number;
+  sulphur: number;
+  nitrogen: number;
+  fluor: number;
+  carbonMonoxide: number;
+  ozone: number;
+}
